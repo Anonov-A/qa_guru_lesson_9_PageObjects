e10b2e6272ce19cf36ef60a0e8e9896522928816
Anonov_A <79769699+Anonov-A@users.noreply.github.com>
2026-01-01 16:47:40 +0500

    Update conftest.py
    Update conftest

--- conftest.py
+++ conftest.py
@@ -1,15 +1,25 @@
+from selenium import webdriver
 import pytest
 from selene import browser
-from selenium import webdriver
-
+import tempfile
+import shutil
 
 @pytest.fixture(scope='function')
 def browser_management():
+    # Создаем уникальную временную директорию для user data
+    temp_dir = tempfile.mkdtemp()
+    
     browser.config.base_url = 'https://demoqa.com'
     browser.config.driver_options = webdriver.ChromeOptions()
     browser.config.driver_options.add_argument('--window-size=1920,1080')
+    browser.config.driver_options.add_argument('--no-sandbox')
+    browser.config.driver_options.add_argument('--disable-dev-shm-usage')
+    browser.config.driver_options.add_argument('--headless')  # для Jenkins
+    browser.config.driver_options.add_argument('--incognito')  # инкогнито режим
+    
     browser.config.timeout = 20
 
     yield
 
     browser.quit()
+    shutil.rmtree(temp_dir, ignore_errors=True)
