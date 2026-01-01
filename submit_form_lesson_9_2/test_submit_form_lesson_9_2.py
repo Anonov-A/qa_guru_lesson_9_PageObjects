d3faff8cbb1eb71e763fb4486ff9ccbc2f3a35b6
Anonov_A <79769699+Anonov-A@users.noreply.github.com>
2026-01-01 16:49:10 +0500

    Update test_submit_form_lesson_9_2.py

--- submit_form_lesson_9_2/test_submit_form_lesson_9_2.py
+++ submit_form_lesson_9_2/test_submit_form_lesson_9_2.py
@@ -8,14 +8,22 @@ from submit_form_lesson_9_2.pages.pages import RegistrationPage
 
 @pytest.fixture(scope='function')
 def browser_management():
+    from selenium import webdriver
+    from selene import browser
+    
     browser.config.base_url = 'https://demoqa.com'
     browser.config.driver_options = webdriver.ChromeOptions()
     browser.config.driver_options.add_argument('--window-size=1920,1080')
+    browser.config.driver_options.add_argument('--no-sandbox')
+    browser.config.driver_options.add_argument('--disable-dev-shm-usage')
+    browser.config.driver_options.add_argument('--headless')
+    browser.config.driver_options.add_argument('--incognito')  # или --no-user-data-dir
     browser.config.timeout = 30
 
     yield
 
     browser.quit()
+
 
 
 def test_registration_alexey(browser_management):
