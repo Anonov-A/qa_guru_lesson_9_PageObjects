5b433cb36011b5c6d75dfad1e114fe8274245ef4
Anonov_A <79769699+Anonov-A@users.noreply.github.com>
2026-01-02 00:39:23 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,30 +1,28 @@
-# conftest.py
-import pytest
 from selenium import webdriver
-from selene import browser
-import tempfile
-import shutil
+from selenium. webdriver.chrome.options import Options
+from selene import Browser, Config
 
-@pytest.fixture(scope='function')
-def browser_management():
-    
-    temp_dir = tempfile.mkdtemp()
-    
-    options = webdriver.ChromeOptions()
-    options.add_argument('--window-size=1920,1080')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--headless')
-    options.add_argument('--incognito') 
-    options.add_argument(f'--user-data-dir={temp_dir}')
-    
-    browser.config.driver_options = options
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.timeout = 20
 
-    yield
 
-    browser.quit()
-    
-    # Очистка временной директории
-    shutil.rmtree(temp_dir, ignore_errors=True)
+new
+@pytest. fixture(scope=' function')
+def setup_browser (request):
+    options = Options)
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid: options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+options.capabilities.update(selenoid_capabilities)
+driver = webdriver. Remote(
+    command_executor=f"https://user1:1234@seLenoid.autotests.cloud/wd/hub", 
+    options=options
+)
+
+browser = Browser(Config(driver))
+yield browser
+
+browser.quit()
